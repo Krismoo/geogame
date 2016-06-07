c2a3a02983906d6220d62ae76056b38a4475ce0e
Nicolas Novak <nicolas.novak@students.fhnw.ch>
2016-06-07 14:35:36 +0200

    Update Highscore, fixed missing files

--- script/app.js
+++ script/app.js
@@ -2,6 +2,8 @@
  * Place for all the global variables like config stuff
  * TODO: better to put the variables in the vars they belong?
  *          => like config stuff into "config"
+ *
+ *  BIG TODO: alles in funktionen! open...Screen Funtkionen auf top-Ebene!!!
  */
 var iHaveAbsoluteNofunctionalityIswearOnMeMomMate = 0;
 var range = [5, 15]; //default range beetween range[0] and range[1] (in km)
@@ -39,14 +41,14 @@ var login = {
         var loginScreen = "<header><h1 class='headerText'>GEO GAME</h1></header>" +
             "<section class='loginSize panel panel-default panel-body'>" +
             "<form>" +
-            "<div class='input-group input-group-lg'>" +
+            "<section class='input-group input-group-lg'>" +
             "<h4  class='input-group-addon input-addon-login'>Benutzername: </h4>" +
             "<input class='form-control' id='benutzernameInput' placeholder='Benutzername' type='text'/></br>" +
-            "</div>" +
-            "<div class='input-group input-group-lg'>" +
+            "</section>" +
+            "<section class='input-group input-group-lg'>" +
             "<h4  class='input-group-addon input-addon-login'>Passwort: </h4>" +
             "<input class='form-control' id='passwortInput' placeholder='Passwort' type='password'/></br>" +
-            "</div>" +
+            "</section>" +
             "<input id='registrierenButton' type='submit' value='Registrieren' class='btn btn-lg btn-default'/>" +
             "<input id='loginButton' type='submit' value='Login' class='btn btn-lg btn-default' style='float: right'/>" +
             "</form>" +
@@ -83,25 +85,25 @@ var login = {
         var registerForm = "<header><h4  class='headerText'>Registrieren</h4></header>" +
             "<section  class='center loginSize'>" +
             "<form>" +
-            "<div class='input-group'>" +
+            "<section class='input-group'>" +
             "<p class='input-group-addon input-addon-login'>Benutzername:</p>" +
             "<input class='form-control' id='benutzernameInput' type='text'>" +
-            "</div>" +
-            "<div class='input-group'>" +
+            "</section>" +
+            "<section class='input-group'>" +
             "<p class='input-group-addon input-addon-login'>Passwort:</p>" +
             "<input class='form-control' id='passwortInput' type='password'>" +
-            "</div>" +
-            "<div class='input-group'>" +
+            "</section>" +
+            "<section class='input-group'>" +
             "<p class='input-group-addon input-addon-login'>Passwort wiederholen:</p>" +
             "<input class='form-control' id='passwortInput' type='password'>" +
-            "</div>" +
+            "</section>" +
             "<input id='registrierungAbschickenButton' type='submit' value='Registrieren' class='btn btn-default'>" +
             "</form>" +
             "</section>";
-        var registrierenPopup = "<div id='createUserModal' class='modal'>" +
-            "<div class='modal-content'>" +
-            "<span id='closeModal'>x</span>" + registerForm + "</div>" +
-            "</div>";
+        var registrierenPopup = "<section id='createUserModal' class='modal'>" +
+            "<section class='modal-content'>" +
+            "<p id='closeModal'>x</p>" + registerForm + "</section>" +
+            "</section>";
         $("main").append(registrierenPopup);
 
         //TODO: ajax um Benutzernamen bereits benutzt zu überprüfen?? (onChange)
@@ -114,7 +116,8 @@ var login = {
 
         modal.style.display = "block";
 
-        // When the user clicks on <span> (x), close the modal
+        // When the user clicks on x, close the modal
+        //TODO: there are no spans!
         span.onclick = function () {
             event.preventDefault();
             modal.style.display = "none";
@@ -134,6 +137,7 @@ var login = {
      */
     loginCheck: function (benutzername, passwort) {
         //TODO: database check user&&pw
+
         var successful = true;
         if (benutzername === "false") {
             successful = false;
@@ -210,29 +214,29 @@ var game = {
 
         var gameScreen = "<header><h3 class='headerText'>Go to the place on this Picture" +
             "<button id='zurueckButtonG' class='menubtn btn btn-default'>Menu</button></h3></header>" +
-            "<section>" +
-            "<div id='myCarousel' class='carousel slide' data-ride='carousel'></div>" +
-            "<div class='menu'></div>" +
-            "</section>";
+            "<p>" +
+            "<p id='myCarousel' class='carousel slide' data-ride='carousel'></p>" +
+            "<p class='menu'></p>" +
+            "</p>";
 
         var carouselNode = "" +
-            "<ol class='carousel-indicators'>" +
+            "<p class='carousel-indicators'>" +
             "<!-- Indicators! -->" +
-            "</ol>" +
+            "</p>" +
 
             "<!-- Wrapper for slides -->" +
-            "<div class='carousel-inner' role='listbox'>" +
+            "<section class='carousel-inner' role='listbox'>" +
             "<!--Pictures! -->" +
-            "</div>" +
+            "</section>" +
 
             "<!-- Left and right controls -->" +
             "<a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'>" +
-            "<span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>" +
-            "<span class='sr-only'>Previous</span>" +
+            "<p class='glyphicon glyphicon-chevron-left' aria-hidden='true'></p>" +
+            "<p class='sr-only'>Previous</p>" +
             "</a>" +
             "<a class='right carousel-control' href='#myCarousel' role='button' data-slide='next'>" +
-            "<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>" +
-            "<span class='sr-only'>Next</span>" +
+            "<p class='glyphicon glyphicon-chevron-right' aria-hidden='true'></p>" +
+            "<p class='sr-only'>Next</p>" +
             "</a>";
 
         var pictureNodes = "";
@@ -252,20 +256,21 @@ var game = {
             //TODO: http://www.w3schools.com/bootstrap/bootstrap_carousel.asp
             //TODO: Indicator (circle at the bottom) doesnt work
             if (i === 1) {
-                pictureNodes += "<div class='item item-active active'>" +
+                pictureNodes += "<p class='item item-active active'>" +
                     "<img src='Assets/Pictures/GeoGame_Picture_" + number + ".jpg' alt='Picture'>" +
-                    "</div>";
+                    "</p>";
                 indicatorNodes += "<li data-target='#myCarousel' data-slide-to='" + (i-1) + "' class='active'></li>";
             } else {
-                pictureNodes += "<div class='item'>" +
+                pictureNodes += "<p class='item'>" +
                     "<img src='Assets/Pictures/GeoGame_Picture_" + number + ".jpg' alt='Picture'>" +
-                    "</div>";
+                    "</p>";
                 indicatorNodes += "<li data-target='#myCarousel' data-slide-to='" + (i - 1) + "'></li>";
 
             }
-
         }
-        var points = 666;//game.getPointsOfActualGame();
+        var points = 666;
+       // getPointsOfActualGame();
+
         var menuNode = "<input id='checkLocationButton' type='submit' value='Check Location' class='btn btn-lg btn-default'/>" +
             "<h3>Points: " + points + "</h3>" +
             "<input id='giveAHint' type='submit' value='Give me a hint' class='btn btn btn-default'/><br/>" +
@@ -318,9 +323,10 @@ var game = {
 
     //TODO: IMPLEMENT "getPointsOfActualGame"
     getPointsOfactualGame: function () {
+        return 666;
     },
 
-    //TODO: IMPLEMENT "loadPictures"
+    //TODO: IMPLEMENT "checkLocation"
     checkLocation: function () {
     }
 }
@@ -334,31 +340,19 @@ var highscore = {
      */
     openHighscorescreen: function () {
         eraseScreen();
-        var highscoreData = highscore.loadHighscores();
         var highscorescreen = "<header><h3 class='headerText'>Highscore</h3></header>" +
             "<section class='contentSize'>" +
                 //Two hours for this stupid, stupidly awesome looking Table LOL
             "<table class='center table-responsive panel panel-primary'>" +
-            "<thead class='panel-heading'><tr><th>Platz</th><th>Name</th><th class='bigTd'>Datum</th><th class='bigTd'>Punkte</th></tr></thead>" +
-            "<tbody class='panel-body'>" +
-            "<tr><td>1</td><td>Simon</td><td class='bigTd'>29.05.2016</td><td class='bigTd'>66666</td></tr>" +
-            "<tr><td>2</td><td>Dominic</td><td class='bigTd'>28.05.2016</td class='bigTd'><td>6666</td></tr>" +
-            "<tr><td>3</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>666</td></tr>" +
-            "<tr><td>4</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>66</td></tr>" +
-            "<tr><td>5</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>6</td></tr>" +
-            "<tr><td>6</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>0.6</td></tr>" +
-            "<tr><td>7</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>0.06</td></tr>" +
-            "<tr><td>8</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>0.006</td></tr>" +
-            "<tr><td>9</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>0.0006</td></tr>" +
-            "<tr><td>10</td><td>Nicolas</td><td class='bigTd'>27.05.2016</td class='bigTd'><td>0.00006</td></tr>" +
-            "</tbody>" +
+            "<thead class='panel-heading'><tr><th>Platz</th><th>Name</th><th class='bigTd'>Punkte</th></tr></thead>" +
+            "<tbody class='panel-body'></tbody>" +
             "</table>" +
-            "<br/><div class='buttonContainer'>" +
+            "<br/><p class='buttonContainer'>" +
             "<button id='zurueckButtonH' class='button btn btn-default'>Zurück</button>" +
-            "</div></section>";
+            "</p></section>";
         $("main").append(highscorescreen);
-        //TODO: make a foreach which appends the rows
 
+        highscore.loadHighscores();
 
         $('body').on('click', '#zurueckButtonH', function (event) {
             event.preventDefault();
@@ -368,8 +362,27 @@ var highscore = {
     },
 
     loadHighscores: function () {
-        //TODO: load ALLLLL the highsocredata
+        //TODO: OMG JUST REMOVE THEM BEFORE ADDING THEM AGAIN!!!
+        $("tr.content").remove();
 
+        var i = 1;
+        $.ajax({
+            url: "api/highscore",
+            data: {
+            },
+            dataType: "json",
+            success: function (data) {
+                $.each(data, function () {
+                    console.log(data);
+                    var $row = $("<tr class='content'><td>"+i+"</td><td>"+data[i-1]['name']+"</td><td class='bigTd'>"+data[i-1]['score']+"</td></tr>");
+                    $(".panel-body").append($row);
+                    i++;
+                });
+            },
+            error: function () {
+                $(".panel-body tr").remove();
+            }
+        });
     }
 }
 
@@ -385,25 +398,25 @@ var config = {
         var configscreen = "<header><h3 class='headerText'>Config</h3></header>" +
             "<section class='contentSize'>" +
             "<form>" +
-            "<div class='center input-group input-config'>" +
+            "<section class='center input-group input-config'>" +
             "<p class='input-group-addon input-addon-login2'>Schwierigkeit: </p>" +
             "<select id='difficulty' class='form-control'>" +
             "<option value='easy'>Leicht</option>" +
             "<option value='middle'>Mittel</option>" +
             "<option value='difficult'>Schwer</option>" +
             "</select> " +
-            "</div>" +
-            "<div class='center panel panel-default'>" +
-            "<p class='panel-heading'>" +
+            "</section>" +
+            "<section class='center panel panel-default'>" +
+            "<section class='panel-heading'>" +
             "<label for='range'>Reichweite:</label>" +
             "<input type='text' id='range' readonly >" +
-            "</p>" +
-            "<div class='panel-body'><div id='slider-range'></div></div>" +
-            "</div></form>" +
-            "<br/><div class='buttonContainer'>" +
+            "</section>" +
+            "<section class='panel-body'><p id='slider-range'></p></p>" +
+            "</section></form>" +
+            "<p class='buttonContainer'>" +
             "<button id='saveConfigButton' class='button btn btn-default'>Speichern</button><br/>" +
             "<button id='zurueckButtonC' class='button btn btn-default'>Zurück</button>" +
-            "</div></section>";
+            "</p></section>";
         $("main").append(configscreen);
         $("select").val(difficulty);
 
@@ -468,9 +481,9 @@ var about = {
             "<p>Dies ist eine Single Page App, erstellt im Rahmen des Erfahrungsnotenprojektes des Moduls webeC (Web Engineering) and der FHNW in Brugg.</p>" +
             "<p>Developer: </p>" +
             "<ul><li>Simon Rissi</li><li>Dominic Bär</li><li>Nicolas Novak</li></ul>" +
-            "<br/><div class='buttonContainer'>" +
+            "<br/><p class='buttonContainer'>" +
             "<button id='zurueckButtonA' class='button btn btn-default'>Zurück</button>" +
-            "</div></section>";
+            "</p></section>";
         $("main").append(aboutScreen);
 
         $('body').on('click', '#zurueckButtonA', function (event) {
